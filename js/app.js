cb61108beb5bb489b9f0fbfdb4a70fc51420fe6f
mdawood832 <mdawood832@gmail.com>
2022-07-05 20:54:22 -0500

    centered egg and added typing effect

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-console.log('maryam dawood')
+class Tamagotchi {
+    constructor(name){
+        this.name = name
+        this.hunger = 1
+        this.sleepiness = 1
+        this.boredom = 1
+        this.age = 0
+    }
+}
+
+const character = new Tamagotchi (prompt ("what is the name of your Tamagotchi"))
+// console.log(character) THIS WORKS: saves the users input for name into the class under name using 
+// constructor method 
+
+
+
+
+
+
+  
